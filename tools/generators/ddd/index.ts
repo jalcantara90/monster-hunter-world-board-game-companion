28167aaf9497d5ec2071e5996e8af1fa9b652ebb
jalcantara90 <jonathan.alcantara@thetrainline.com>
2023-03-08 00:09:00 +0100

    fix: ddd generator v4

--- tools/generators/ddd/index.ts
+++ tools/generators/ddd/index.ts
@@ -17,18 +17,20 @@ interface DDDSchemaGeneration {
 export default async function (tree: Tree, schema: DDDSchemaGeneration) {
   const projectRoot = readProjectConfiguration(tree, schema.project).root;
 
+  const entityName = schema.name.substring(0, schema.name.length-1);
   generateFiles(
     tree,
     joinPathFragments(__dirname, './files'),
     joinPathFragments(projectRoot, 'src', schema.name),
     {
-      template: names(schema.name).className,
-      className: names(schema.name).className,
-      constant: names(schema.name).constantName,
-      propertyName: names(schema.name).propertyName,
+      template: names(entityName).className,
+      className: names(entityName).className,
+      constant: names(entityName).constantName,
+      propertyName: names(entityName).propertyName,
       tmpl: '',
     }
   );
+
   await moduleGenerator(tree, { name: schema.name, project: schema.project });
   await formatFiles(tree);
   return () => {
