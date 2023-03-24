f706c2cee35b515d412b05d05b4d297cd7e10638
jalcantara90 <jonathan.alcantara@thetrainline.com>
2023-03-24 09:26:13 +0100

    feat: add fonts

--- apps/webapp/src/app/app.tsx
+++ apps/webapp/src/app/app.tsx
@@ -1,51 +1,7 @@
-import { Layout } from './core/Layout';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { BrigadeList } from './brigades/BrigadeList';
-import { CampaignList } from './campaigns/CampaignList';
-import { HunterList } from './hunters/HunterList';
-import { InventoryList } from './inventory/InventoryList';
-import { ArmorList } from './armors/ArmorList';
-import { WeaponList } from './weapons/WeaponList';
-import { MaterialList } from './materials/MaterialList';
-
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <BrigadeList />,
-  },
-  {
-    path: '/campaigns',
-    element: <CampaignList />,
-  },
-  {
-    path: '/hunters',
-    element: <HunterList />,
-  },
-  {
-    path: '/inventory',
-    element: <InventoryList />,
-  },
-  {
-    path: '/armors',
-    element: <ArmorList />,
-  },
-  {
-    path: '/weapons',
-    element: <WeaponList />,
-  },
-  {
-    path: '/material',
-    element: <MaterialList />,
-  },
-]);
+import { Router } from './Router';
 
 export function App() {
-  return (
-    <Layout>
-      <RouterProvider router={routes} />
-    </Layout>
-  );
+  return <Router />;
 }
 
 export default App;
