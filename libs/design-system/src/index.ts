7bdf1cb486eae8e754071315316f408d2be11e97
jalcantara90 <jonathan.alcantara@thetrainline.com>
2023-03-30 19:34:28 +0200

    add: export to design system barrel file

--- libs/design-system/src/index.ts
+++ libs/design-system/src/index.ts
@@ -5,6 +5,8 @@ export { BrigadeCard, BrigadeCardSkeletonList } from './lib/atoms/BrigadeCard';
 export { SectionTitle } from './lib/atoms/SectionTitle';
 export { WeaponIcon } from './lib/atoms/WeaponIcon';
 export { ArmorIcon } from './lib/atoms/ArmorIcon';
+export { Input } from './lib/atoms/forms/Input';
+export { Select } from './lib/atoms/forms/Select';
 
 // Molecules
 export { SideMenu } from './lib/molecules/SideMenu';
