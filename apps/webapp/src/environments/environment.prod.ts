5a8ca27e8f1dab5598747e1e7b251418186d3208
jalcantara90 <jonathan.alcantara@thetrainline.com>
2023-04-13 08:31:38 +0200

    test

--- apps/webapp/src/environments/environment.prod.ts
+++ apps/webapp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:
-    'https://octopus-app-zu2uv.ondigitalocean.app/monster-hunter-world-board-game2/api',
+  // baseUrl:
+  //   'https://octopus-app-zu2uv.ondigitalocean.app/monster-hunter-world-board-game2/api',
 };
